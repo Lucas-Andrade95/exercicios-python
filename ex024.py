be00126a072d74c8a9b5f8e17ae4533fa47bf2bf
Lucas Andrade <104659660+Lucas-Andrade95@users.noreply.github.com>
2023-06-29 17:01:31 -0400

    Create ex024.py

---
+++ ex024.py
@@ -0,0 +1,10 @@
+cidade = str(input('Qual cidade você nasceu? ')).strip()
+c1 = cidade.lower()
+print('santo ' in c1[:6])
+
+
+#frase = input(str('Escreva uma frase: '))
+#print('Essa frase tem {} caracteres.'.format(len(frase)))
+#print('Essa frase tem {} letras "o" '.format(frase.count('o')))
+#print('Essa frase tem o nome Lucas e começa no caractere {}.'.format(frase.find('Lucas')))
+#print('Essa frase tem a palavra "Lucas"? {}'.format('Lucas' in frase))
