a23d207300aa01bca824e292b9f70c834f9a2f0c
Lucas Andrade <104659660+Lucas-Andrade95@users.noreply.github.com>
2023-06-29 16:02:53 -0400

    Update ex023.py

--- ex023.py
+++ ex023.py
@@ -1,6 +1,10 @@
-n = input('Digite um número entre 0 e 9999: ')
-print('Unidade: {}'.format(n[0]))
-print('Dezena: {}'.format(n[1]))
-print('Centena: {}'.format(n[2]))
-print('Milhar: {}'.format(n[3]))
-#SÓ DA CERTO COM NUMEROS ENTRE 1000 E 9999
+num = int(input('Digite um número entre 0 e 9999: '))
+u = num % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando o número {}!'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
