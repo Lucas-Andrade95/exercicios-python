6caf87c21e6e3d4f3f9e06647b35f42ce4245984
Lucas Andrade <104659660+Lucas-Andrade95@users.noreply.github.com>
2023-06-30 08:49:28 -0400

    Update ex025.py

--- ex025.py
+++ ex025.py
@@ -1,2 +1,2 @@
 nome = str(input('Qual o seu nome compelto? ')).strip()
-print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
