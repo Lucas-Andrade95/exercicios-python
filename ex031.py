0a4a72edaf53ad8cdf46453aa297520d3e63b220
Lucas Andrade <104659660+Lucas-Andrade95@users.noreply.github.com>
2023-07-19 21:40:11 -0400

    exercises updates

--- ex031.py
+++ ex031.py
@@ -1,11 +1,14 @@
 #CALCULANDO O PREÇO DA VIAGEM
 
 km = int(input('Qual a distância da viagem em Km? '))
-if km <= 200:
+'''if km <= 200:
     valordapassagem1 = km*0.5
     print ('O valor da passagem será de R$ {:.2f}'.format(valordapassagem1))
 else:
     valordapassagem2 = km*0.45
-    print('O valor da passagem será de R$ {:.2f}!'.format(valordapassagem2))
+    print('O valor da passagem será de R$ {:.2f}!'.format(valordapassagem2))'''
 
+#CONDIÇÃO SIMPLIFICADA
+valordapassagem = km * .50 if km <= 200 else km * 0.45
+print('O valor da passagem será de R$ {:.2f}!'.format(valordapassagem))
 print('--FIM--')
--- ex032.py
+++ ex032.py
@@ -1,8 +1,9 @@
 #O ANO É BISSEXTO?
 
 ano = int(input('Digite um ano: '))
-if ano%4 == 0:
+if ano %4 == 0 and ano %100 != 0 or ano % 400 == 0:
     print('{} é um ano bissexto.'.format(ano))
 else:
     print('{} não é um ano bissexto.'.format(ano))
 print('--FIM--')
+
