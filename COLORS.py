6cab8ed9844201295868c8d1255090ab10f45e58
Lucas Andrade <104659660+Lucas-Andrade95@users.noreply.github.com>
2023-10-19 14:34:26 -0400

    Colors - Código ANSI

---
+++ COLORS.py
@@ -0,0 +1,6 @@
+print('\33[1;31;41mOlá mundo!\033[m')
+print('\33[7mHello World!\033[m')
+print('\033[4;32;42m Oi, mundão véio sem porteira!\033[m')
+print('\033[1;35m HOLA, MONDO! \33[m')
+
+#CÓDIGO ANSI - \033[ Style 0 1 4 7; Text 30 a 37; Background 40 a 47 m ESCREVO O TEXTO AQUI \033[m
--- ex035.py
+++ ex035.py
@@ -1,5 +1,6 @@
 #É POSSÍVEL FORMAR UM TRIÂNGULO?
 
+
 print('Vamos saber se três retas poderão formar um triângulo.')
 a = float(input('Digite o comprimento da primeira reta (cm): '))
 b = float(input('Digite o comprimento da segunda reta (cm): '))
