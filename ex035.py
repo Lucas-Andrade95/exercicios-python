ae63aae9059e5929025527aa4ba1ea56c4cf5987
Lucas Andrade <104659660+Lucas-Andrade95@users.noreply.github.com>
2023-10-19 14:11:54 -0400

    Update ex035.py

--- ex035.py
+++ ex035.py
@@ -4,11 +4,7 @@ print('Vamos saber se três retas poderão formar um triângulo.')
 a = float(input('Digite o comprimento da primeira reta (cm): '))
 b = float(input('Digite o comprimento da segunda reta (cm): '))
 c = float(input('Digite o comprimento da terceira reta (cm):'))
-if (b - c) < a < b + c:
-    print('Sim, é possível formar um triângulo!')
-if (a - c) < b < a + c:
-    print('Sim, é possível formar um triângulo! ')
-if (a - b) < c < a + b:
+if a < (b + c) and b < (a + c) and  c < (a + b):
     print('Sim, é possível formar um triângulo!')
 else:
     print('Não é possível formar um triângulo.')
