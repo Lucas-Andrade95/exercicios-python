5afc3997cf4bf0bcce198bc753bf3937d04ad004
Lucas Andrade <104659660+Lucas-Andrade95@users.noreply.github.com>
2024-01-20 14:12:58 -0400

    Modifying exercise 87

--- ex087.py
+++ ex087.py
@@ -8,12 +8,12 @@ somapares = numero = somaterceiracoluna = 0
 
 for cadaum in matriz:
     for c in range(0, 3):
-        cadaum[c].append(int(input(f'Digite um valor para ')))
+        cadaum[c].append(int(input(f'Digite um valor: ')))
 
 #IMPRIMINDO A MATRIZ:
 print('-'*30)
 for x, y, z in matriz:
-    print(x, y, z)
+    print(f'{x}    {y}    {z}')
 
 #ACESSANDO OS VALORES E SOMANDO OS PARES
 for c in matriz:
@@ -26,7 +26,7 @@ for c in matriz:
 for x, y, z in matriz:
     somaterceiracoluna += z[0]
     
-
+print('-'*30)
 print(f'A soma dos valores pares é igual a {somapares}')
 print(f'A soma da terceira coluna é igual a {somaterceiracoluna}')
 print(f'O maior valor da segunda linha é {max(matriz[1])}')
